7cbddbd92222aee716c0d6ddc776aee510686f40
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-04 11:41:50 -0300

    utilizando encaps em poo

--- Carro.java
+++ Carro.java
@@ -1,4 +1,17 @@
 
 public class Carro {
 
+	public void ligar() {
+		confereCambio();
+		confereCombustivel();
+		System.out.println("CARRO LIGADO");
+	}
+	
+	private void confereCombustivel() {
+		System.out.println("conferindo combustivel");
+	}
+	
+	private void confereCambio() {
+		System.out.println("conferindo campio em P");
+	}
 }
