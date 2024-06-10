c8d8fedc704d67b6d0c94bce2352b2a6cce067e8
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-10 17:04:34 -0300

    abstração em java

--- Menssage/Telegram.java
+++ Menssage/Telegram.java
@@ -2,4 +2,11 @@ package Menssage;
 
 public class Telegram extends ServicoMensagemInstantanea {
 
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem pelo Telegram");
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Telegram");
+	}
+
 }
