0a05496c4323350461c95b7815887515b2ad4746
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-10 16:49:12 -0300

    encapsulamente em java

---
+++ Menssage/ComputadorPedrinho.java
@@ -0,0 +1,11 @@
+package Menssage;
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.enviarMensagem();
+		
+		msn.receberMensagem();
+	}
+}
